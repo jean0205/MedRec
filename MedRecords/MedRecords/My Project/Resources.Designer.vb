cb0a7843e1f515bbd0bf81ea76ed17820cde7dab
Jean Carlos Soto <jeanc.soto020@gmail.com>
2021-08-18 07:49:29 -0400

    fff

--- MedRecords/MedRecords/My Project/Resources.Designer.vb
+++ MedRecords/MedRecords/My Project/Resources.Designer.vb
@@ -133,16 +133,6 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property update() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("update", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
         Friend ReadOnly Property visit() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("visit", resourceCulture)
